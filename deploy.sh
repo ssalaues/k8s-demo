8d7f4344c6fc2a5dc3e83412fed76f20b64aa6f3
Salim <salimsalaues@gmail.com>
2024-01-30 13:15:22 -0800

    testing new pipeline

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -ex
+
+for i in 1 2; do
+cat <<- EOF > service${i}.yml
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+    name: service-${i}
+    labels:
+        app: service-${i}
+    spec:
+    replicas: 1
+    selector:
+        matchLabels:
+        app: service-${i}
+    template:
+        metadata:
+        labels:
+            app: service-${i}
+        spec:
+        containers:
+        - name: server
+            image: test-service:${GITHUB_SHA}
+            command:
+            - bash
+            - -exc
+            - |
+                while true; do
+                echo "server-${i} up"
+                sleep 1
+                done
+EOF
+done
