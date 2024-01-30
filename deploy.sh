3db978b6dd55e10c8c37f9f362ee826828d1c1c4
Salim <salimsalaues@gmail.com>
2024-01-30 14:43:23 -0800

    fix: deploy workflows

--- deploy.sh
+++ deploy.sh
@@ -3,32 +3,32 @@ set -ex
 
 for i in 1 2; do
 cat <<- EOF > service${i}.yml
-    apiVersion: apps/v1
-    kind: Deployment
-    metadata:
+apiVersion: apps/v1
+kind: Deployment
+metadata:
     name: service-${i}
     labels:
         app: service-${i}
-    spec:
+spec:
     replicas: 1
     selector:
         matchLabels:
-        app: service-${i}
+            app: service-${i}
     template:
         metadata:
-        labels:
-            app: service-${i}
+            labels:
+                app: service-${i}
         spec:
-        containers:
-        - name: server
-            image: test-service:${GITHUB_SHA:-latest}
-            command:
-            - bash
-            - -exc
-            - |
-                while true; do
-                echo "server-${i} up"
-                sleep 1
-                done
+            containers:
+              - name: server
+                image: test-service:${GITHUB_SHA:-latest}
+                command:
+                - bash
+                - -exc
+                - |
+                    while true; do
+                    echo "server-${i} up"
+                    sleep 1
+                    done
 EOF
 done
